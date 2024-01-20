e2be6d7d0f75ee56c49045cde202b95886e8b386
Akshat-Vashisht <akashat2003@gmail.com>
2024-01-20 19:48:04 +0530

    Fixed bug in logger.py

--- src/logger.py
+++ src/logger.py
@@ -7,11 +7,11 @@ LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), 'logs', LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE_PATH = os.paths.join(logs_path, LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format='[ %(ascitime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    format='[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
